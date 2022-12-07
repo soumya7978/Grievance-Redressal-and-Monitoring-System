c0b03e36ff96dd1aa72134f7e1b95be8880e7b95
pandasoumya103 <120056479+pandasoumya103@users.noreply.github.com>
2022-12-08 02:39:09 +0530

    Update AdminLogin.java
    package change

--- src/AdminLogin.java
+++ src/AdminLogin.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.*;
 import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
